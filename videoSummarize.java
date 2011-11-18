51b4ba299b020946bd4184f891d4bfb4d9885157
unknown <Manu Bhadoria@ManuBhadoria-PC.(none)>
2011-11-18 00:50:59 -0800

    nothing yet, just created the summarization project

---
+++ videoSummarize.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Manu Bhadoria
+ */
+public class videoSummarize {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
