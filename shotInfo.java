105c17b7c37c1e325733564f8e48a76950575c28
unknown <Manu Bhadoria@ManuBhadoria-PC.(none)>
2011-11-20 00:14:06 -0800

    added HashMap for shot, and implemented color based Algo for computing shots

---
+++ shotInfo.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Manu Bhadoria
+ */
+class shotInfo {
+    int numFrames;
+    List<Integer> keyFrames = new ArrayList<Integer>();
+}
--- videoSummarize.java
+++ videoSummarize.java
@@ -1,3 +1,6 @@
+
+import java.io.FileNotFoundException;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -12,7 +15,30 @@ public class videoSummarize {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
+        // get the command line parameters
+        if (args.length < 3) {
+            System.err.println("Invalid number of arguments");
+            return;
+        }
+        String videoFileName = args[0];
+        String audioFileName = args[1];
+        int percentage = Integer.parseInt(args[2]);
+        
+        //Thread soundShotThread = new Thread(new PlaySound(audioFileName));
+        Thread videoShotThread = new Thread(new videoToShots(videoFileName));
+        
+        //soundShotThread.start();
+        videoShotThread.start();
+
+        try {
+            //delay for one second
+            //Thread.sleep(500000);
+            //soundShotThread.join();
+            videoShotThread.join();
+            System.out.println("Main exiting..");
+        } catch (InterruptedException e) {
+        }
     }
 }
