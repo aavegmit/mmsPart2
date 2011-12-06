9cdd7371c81c0848275557b2459a6e97bfd809bd
unknown <Manu Bhadoria@ManuBhadoria-PC.(none)>
2011-12-05 18:10:29 -0800

    changed the random macroblocks to static macroblocks

--- videoToShots.java
+++ videoToShots.java
@@ -260,7 +260,7 @@ public class videoToShots implements Runnable {
                 count =  (int)(videoToShots.numFrames - key);
             }
             objTemp.numFrames = count;
-            objTemp.keyFrames.put(keyFrameNum, 1);
+            //objTemp.keyFrames.put(keyFrameNum, 1);
             shotHashMap.put(key, objTemp);
         } else {
             obj.numFrames += count;
@@ -271,10 +271,10 @@ public class videoToShots implements Runnable {
                 int temp = 0;
                 if(obj.keyFrames.containsKey(keyFrameNum)){
                     temp = obj.keyFrames.get(keyFrameNum);
-                  obj.keyFrames.put(keyFrameNum, temp+1);
+                  //obj.keyFrames.put(keyFrameNum, temp+1);
                 }
                 else {
-                    obj.keyFrames.put(keyFrameNum, 1);
+                    //obj.keyFrames.put(keyFrameNum, 1);
                 }
             }
             shotHashMap.put(key, obj);
